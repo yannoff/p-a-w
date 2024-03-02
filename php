yannoff/p-a-w

commit 3e482b48616970908955e61402902a09da1c4ce9

[runtime] Support PHP files invocation

php
@@ -32,9 +32,9 @@ args=()
 
 if [ "$#" -gt "0" ]
 then
-    # If first arg is an option, assume "php" is implicit
-    # This allow calling the script in the form: bin/php -a
-    if [ "${1#-}" != "$1" ]
+    # If first arg is an option or a php file, assume "php" is implicit
+    # This allow calling the script in the form: "php -a" or "php foo.php"
+    if [ "${1#-}" != "$1" ] || grep '<?' "${1}" >/dev/null 2>&1
     then
         set -- php "$@"
     else

commit 29a617794565246f96a305877c872b48bbbdbf68

[bugfix] Always mount absolute executable paths
- Fixes bug introduced in 3e482b48616970908955e61402902a09da1c4ce9

php
@@ -32,14 +32,14 @@ args=()
 
 if [ "$#" -gt "0" ]
 then
+    # If first arg is an absolute path, mount it
+    [ "${1:0:1}" = "/" ] && args+=( -v ${1}:${1} )
+
     # If first arg is an option or a php file, assume "php" is implicit
     # This allow calling the script in the form: "php -a" or "php foo.php"
     if [ "${1#-}" != "$1" ] || grep '<?' "${1}" >/dev/null 2>&1
     then
         set -- php "$@"
-    else
-        # If first arg is an absolute path, mount it
-        [ "${1:0:1}" = "/" ] && args+=( -v ${1}:${1} )
     fi
 else
     # If no command or option specified, open a bash session